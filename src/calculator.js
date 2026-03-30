197d05eeb181c8a1d37615494c7c4f72caa871ab
Alicelspires <alicelspires@gmail.com>
2026-03-30 19:51:41 -0300

    feat: atualizacao da pipeline e criacao do layout da calculadora

--- src/calculator.js
+++ src/calculator.js
@@ -1,3 +1,36 @@
+function main(){
+    let answer = document.getElementById('result');
+    let btn = document.getElementById('btn');
+
+    btn.addEventListener('click', () => {
+        let primeiroValor = parseFloat(document.getElementById('num1').value);
+        let segundoValor = parseFloat(document.getElementById('num2').value);
+        let operation = document.getElementById('operation').value;
+
+        let result = calculator(
+            primeiroValor, 
+            segundoValor, 
+            operation
+        );
+
+        answer.textContent = result;
+        console.log(result);
+    })
+}
+
+function calculator(a, b, operation) {
+    switch (operation) {
+        case 'sum': 
+            return sum(a, b);
+        case 'subtract':
+            return subtract(a, b);
+        case 'multiply':
+            return multiply(a, b);
+        case 'divide':
+            return divide(a, b);
+    }
+}
+
 function sum(a, b) {
     return a + b;
 }
@@ -7,9 +40,17 @@ function subtract (a ,b) {
 function multiply(a, b) {
     return a * b;
 }
+function divide(a, b) {
+    return a / b;
+}
+
+if(typeof document !== 'undefined') {
+    main();
+}
 
 module.exports = {
     sum,
     subtract,
-    multiply
+    multiply,
+    divide
 };
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,4 +1,4 @@
-const { sum, subtract, multiply } = require('../src/calculator');
+const { sum, subtract, multiply, divide } = require('../src/calculator');
 
 test('deve somar 2 + 3 e retornar 5', () => {
     expect(sum(2, 3)).toBe(5);
@@ -12,3 +12,6 @@ test('deve multiplicar 3 * 5 e retornar 15', () => {
     expect(multiply(3, 5)).toBe(15);
 });
 
+test('deve dividir 10 / 2 e retornar 5', () => {
+    expect(divide(10, 2)).toBe(5);
+});
